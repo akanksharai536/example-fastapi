66bd72495c219848f9ca7ea481b5f00c47d427db
akanksharai <akanksharai536@gmail.com>
2025-04-21 20:10:38 +0200

    again commiting

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     database_name: str
     database_username: str
     secret_key: str
-    algorithm: str
+    algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
 
     class Config:
